d9777e74a29bd0bafb0a11c05d31eec6bccad70a
ivorobyev <vorobyevilia@gmail.com>
2020-12-30 19:06:22 +0300

    editing architecture

--- get_products.py
+++ get_products.py
@@ -1,10 +1,22 @@
 import argparse
+import numpy as np
+from keras.models import load_model
+import pandas as pd
 
 if __name__ == '__main__':
-    start_time: float = time.time()
 
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-u', '--user_id', help='user_id')
-    group.add_argument('-p', '--products', help='number of products to recommend')
+    group.add_argument('-u', '--user_id', help='user_id', type = int)
+    parser.add_argument('-p', '--products', help='tnumber of products to recommend', type=int, default=10)
     args = parser.parse_args()
+
+    model = load_model('ncf.h5')
+    users = pd.read_csv('users_meta.csv')
+    products = pd.read_csv('products_meta.csv')
+    user_num = users['user_id_num'].loc[users['user_id'] == args.user_id].values[0]
+    products_num = np.array(products.product_id_num.to_list())
+    users_num = np.array([user_num] * len(products_num))
+    ratings = model.predict([users_num, products_num])
+    products['rating'] = ratings.flatten()
+    print(products.sort_values(by = 'rating'))
