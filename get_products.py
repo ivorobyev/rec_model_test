3d708573e7b8e89d435a8b44cb2b6226cef5bb54
ivorobyev <vorobyevilia@gmail.com>
2020-12-31 10:48:41 +0300

    editing inference

--- get_products.py
+++ get_products.py
@@ -1,22 +1,34 @@
 import argparse
 import numpy as np
+from numpy import savetxt
 from keras.models import load_model
 import pandas as pd
+import csv
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-u', '--user_id', help='user_id', type = int)
-    parser.add_argument('-p', '--products', help='tnumber of products to recommend', type=int, default=10)
+    parser.add_argument('-p', '--products', help='number of products to recommend', type=int, default=10)
     args = parser.parse_args()
 
     model = load_model('ncf.h5')
     users = pd.read_csv('users_meta.csv')
     products = pd.read_csv('products_meta.csv')
-    user_num = users['user_id_num'].loc[users['user_id'] == args.user_id].values[0]
     products_num = np.array(products.product_id_num.to_list())
-    users_num = np.array([user_num] * len(products_num))
-    ratings = model.predict([users_num, products_num])
-    products['rating'] = ratings.flatten()
-    print(products.sort_values(by = 'rating'))
+
+    recs = []
+    users_count = len(users)
+    for ind, a in users.iterrows():
+        user_num = a['user_id_num']
+        ratings = model.predict([np.array([user_num]*len(products_num)), 
+                                 products_num])
+        products['rating'] = ratings.flatten()
+        user_products = products.sort_values(by = 'rating', ascending=False).head(10)
+        user_products = user_products['product_id'].to_list()
+        user_products = [a['user_id']] + user_products
+        recs.append(user_products)
+        print('{0} of {1} users proceeded'.format(ind+1, users_count))
+        with open('submission.csv', 'a') as fh:
+            print(' '.join(str(x) for x in user_products), file=fh)
+
+    print('done')
