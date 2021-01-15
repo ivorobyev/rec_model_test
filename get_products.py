c8e3b33f7999d3466a1b7e9514c721825fecbbca
ivorobyev <vorobyevilia@gmail.com>
2021-01-15 13:28:01 +0300

    add user_based filtering

--- get_products.py
+++ get_products.py
@@ -1,34 +1,56 @@
-import argparse
 import numpy as np
-from numpy import savetxt
 from keras.models import load_model
 import pandas as pd
 import csv
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import OneHotEncoder
+
+def get_similar_products(user_id, products, g, number):
+    user_profile = np.dot(products[g[g.user_id == user_id]['product_id'].values-1].T, 
+                          g[g.user_id == user_id]['product_id_count'].values)
+    C = cosine_similarity(np.atleast_2d(user_profile), products)
+    R = np.argsort(C)[:, ::-1]
+    return R[0][:number]
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--products', help='number of products to recommend', type=int, default=10)
-    args = parser.parse_args()
-
+    print('collecting data')
     model = load_model('ncf.h5')
     users = pd.read_csv('users_meta.csv')
-    products = pd.read_csv('products_meta.csv')
-    products_num = np.array(products.product_id_num.to_list())
+    products = pd.read_csv('products.csv')
+    transactions_df =  pd.read_csv('/content/drive/My Drive/transactions.csv')
+
+    print('product encoding')
+    one_hot_encoder = OneHotEncoder(sparse=False)
+    aisle_enc = one_hot_encoder.fit_transform(products.aisle_id.values.reshape(-1,1))
+    department_enc = one_hot_encoder.fit_transform(products.department_id.values.reshape(-1,1))
+    prod_enc = np.hstack([aisle_enc, department_enc])
+
+    user_product = transactions_df.groupby(['user_id', 'product_id'])['product_id'].count()
+    user = transactions_df.groupby(['user_id'])['product_id'].count()
+
+    print('compute produts ratings')
+    g = pd.DataFrame(user_product.div(user, level='user_id')).add_suffix('_count').reset_index()
 
     recs = []
     users_count = len(users)
+
     for ind, a in users.iterrows():
         user_num = a['user_id_num']
-        ratings = model.predict([np.array([user_num]*len(products_num)), 
-                                 products_num])
-        products['rating'] = ratings.flatten()
-        user_products = products.sort_values(by = 'rating', ascending=False).head(10)
-        user_products = user_products['product_id'].to_list()
-        user_products = [a['user_id']] + user_products
-        recs.append(user_products)
+
+        #take 100 similar products to user, content-base filtration
+        s_prods = get_similar_products(a['user_id'], prod_enc, g, 100)
+        s_prods = list(map(lambda x: x+1, s_prods))
+
+        #predict by NCF
+        ratings = model.predict([np.array([user_num]*len(s_prods)), np.array(s_prods)])
+        prod_rating = list(zip(s_prods, ratings.flatten()))
+        prod_rating.sort(key = lambda x: x[1], reverse = True)
+        prod_rating = [a['user_id']] + [row[0] for row in prod_rating]
+
+        #write to file
         print('{0} of {1} users proceeded'.format(ind+1, users_count))
         with open('submission.csv', 'a') as fh:
-            print(' '.join(str(x) for x in user_products), file=fh)
+            print(str(prod_rating[0]) + ',' +' '.join(str(x) for x in prod_rating[1:11]), file=fh)
 
     print('done')
